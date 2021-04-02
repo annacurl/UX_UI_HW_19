0ff7915e9fe815d73ff38968a27d698667141c26
annacurl <annaleighcurl@gmail.com>
2021-04-01 21:11:33 -0500

    t

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("index file loaded correctly");
+
+$(document).ready(function(){
+
+    $(".menu").on("click", function(){
+      // $(".menu").fadeToggle("slow").toggleClass("menu-hide");
+      $(".list-item").click("menu-hide);
+      }
+        height: 'toggle'
+      });
+  
+    });
